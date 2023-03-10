1058f97804e3d33d256a4c9a3fffa2846eabb286
Marius Franck <mariusfranck20@gmail.com>
2023-03-10 17:18:08 +0100

    Email Unique Primary Key

--- Model/database.sql
+++ Model/database.sql
@@ -1,13 +1,20 @@
-DROP TABLE IF EXISTS "user";
+DROP TABLE IF EXISTS "user", "trajets" CASCADE;
 
-CREATE TABLE "user" (
-  email VARCHAR(255) UNIQUE NOT NULL,
-  username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  PRIMARY KEY (email)
+CREATE TABLE "user"
+(
+    email    VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(255)        NOT NULL,
+    password VARCHAR(255)        NOT NULL,
+    PRIMARY KEY (email)
 );
 
-
-CREATE TABLE "itineraire" (
-  
+CREATE TABLE "trajets"
+(
+    id      SERIAL PRIMARY KEY,
+    depart  VARCHAR(255) NOT NULL,
+    arrivee VARCHAR(255) NOT NULL,
+    date    DATE         NOT NULL,
+    prix    INTEGER      NOT NULL,
+    email   VARCHAR(255) NOT NULL,
+    FOREIGN KEY (email) REFERENCES "user" (email)
 );
