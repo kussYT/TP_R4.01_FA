f7bee8bfec6adfbc4b7762cd4453a58a8d1b0122
Marius Franck <mariusfranck20@gmail.com>
2023-03-10 17:00:02 +0100

    Email Unique Primary Key

--- Model/database.sql
+++ Model/database.sql
@@ -1,9 +1,8 @@
 DROP TABLE IF EXISTS "user";
 
 CREATE TABLE "user" (
-  id INT NOT NULL,
-  email VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (email)
 );
