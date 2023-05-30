ad3150095917b0fd1eff937b1017aa7e1645624a
Marius Franck <mariusfranck20@gmail.com>
2023-05-30 10:56:21 +0200

    Favoris

--- Controller/preferenceController.php
+++ Controller/preferenceController.php
@@ -1,5 +1,19 @@
 <?php
+class preferenceController {
+    private $preferenceModel;
 
-class preferenceController{
+    public function __construct($preferenceModel) {
+        $this->preferenceModel = $preferenceModel;
+    }
 
+    // Méthode pour traiter les préférences renseignées par l'utilisateur lors de la saisie d'un trajet
+    public function processPreferencesAction($preferences) {
+        // Appeler la méthode du modèle pour traiter les préférences
+        $processedPreferences = $this->preferenceModel->processPreferences($preferences);
+
+        // Autres actions à effectuer en fonction des préférences traitées
+        // Par exemple, mettre à jour d'autres informations du trajet, afficher un message de confirmation, etc.
+
+        // Rediriger ou renvoyer une réponse appropriée à l'utilisateur
+    }
 }
